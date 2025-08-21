97d9bdc93a49ca6f4a801b7cb7f786fa9acba224
Pénélope PIGEAT <penelopepigeat@MacBook-Pro-de-Penelope.local>
2025-08-21 19:56:24 +0200

    home page

--- src/pages/PageHome/Home.jsx
+++ src/pages/PageHome/Home.jsx
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 
-//import Modal from "../../components/ModalComponent/Modal.jsx";
-
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home-container">
       <main className="main-content"></main>
-
-      <Modal show={modalOpen} onClose={() => setModalOpen(false)} />
     </div>
   );
 };
